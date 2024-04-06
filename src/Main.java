d253e1b98f6769c47d2d1e4af7cd308ecda9bcbc
JTK1701 <jonmalkon@mail.ru>
2024-04-06 22:56:49 +0300

    hw6

--- src/Main.java
+++ src/Main.java
@@ -4,38 +4,46 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("1 задача");
         for (int i = 1; i <= 10; i++ ){
-            System.out.println("покажи мне " + i);
+            System.out.print(i + " ");
         }
+        System.out.println(" ");
         System.out.println(" ");
         System.out.println("2 задача");
         for (int i = 10; i > 0; i--){
-            System.out.println("тык " + i);
+            System.out.print(i + " ");
         }
         System.out.println(" ");
+        System.out.println(" ");
         System.out.println("3 задача");
+        System.out.println("четные числа от 0 до 17:");
         for (int y = 0; y < 17; y += 2 ){
-            System.out.println("четные, пажаласта " + y );
+            System.out.print( y + " ");
         }
+        System.out.println(" ");
         System.out.println(" ");
         System.out.println("4 задача");
         for (int i = 10; i >= -10; i--){
-            System.out.println(i);
+            System.out.print(i + " ");
         }
+        System.out.println(" ");
         System.out.println(" ");
         System.out.println("5 задача");
         for (int i = 1994; i <= 2096; i += 4){
             System.out.println( i + " год является високосным");
         }
         System.out.println(" ");
+        System.out.println(" ");
         System.out.println("6 задача");
         for (int i = 7; i <= 98; i += 7){
-            System.out.println(i);
+            System.out.print(i + " ");
         }
+        System.out.println(" ");
         System.out.println(" ");
         System.out.println("7 задача");
         for (int i = 1; i <= 512; i *= 2) {
-            System.out.println( i );
+            System.out.print(i + " ");
         }
+        System.out.println(" ");
         System.out.println(" ");
         System.out.println("8 задача");
         int money = 29000;
@@ -44,6 +52,7 @@ public class Main {
             money += 29000;
         }
         System.out.println(" ");
+        System.out.println(" ");
         System.out.println("9 задача");
         float money9 = 29000f;
         for (int i = 1; i <= 12; i++) {
@@ -51,6 +60,7 @@ public class Main {
             money9 += money9/100;
             money9 += 29000;
         }
+        System.out.println(" ");
         System.out.println(" ");
         System.out.println("10 задача ");
         for (int i = 1; i <= 10; i++) {
